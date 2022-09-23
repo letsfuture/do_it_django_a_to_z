4d60942bfc52eab28799ec1d97a5bb3ce3f60232
letsfuture <future220616@gmail.com>
2022-09-23 17:00:41 +0900

    장고 학습 중간본

--- blog/models.py
+++ blog/models.py
@@ -53,7 +53,7 @@ class Post(models.Model):
         return os.path.basename(self.file_upload.name)
 
     def get_file_ext(self):
-        return self.get_File_name().split('.')[-1]
+        return self.get_file_name().split('.')[-1]
 
     def get_content_markdown(self):
         return markdown(self.content)
--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/mdeia/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
